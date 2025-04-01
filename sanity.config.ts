f8f543d65953537485c9101f51cd9784a7bff43f
Rune_Cabbeke <cabbeke.rune@gmail.com>
2025-04-01 09:43:56 +0200

    Update Sanity project ID in configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   name: "default",
   title: "MC MIKKOLA CMS",
 
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "dXTTN1MR3mwnTbqkclsBtYAI",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "0u29u9fd",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
 
   plugins: [deskTool(), visionTool()],
