5861e6bea5de4a82789c207df43142a41fd19c55
Rune_Cabbeke <cabbeke.rune@gmail.com>
2025-04-04 12:24:50 +0200

    Remove obsolete Sanity Studio files and configurations

--- mc-mikkola-studio/mc-mikkola-studio/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import type { SchemaTypeDefinition } from "sanity"
-import article from "./article"
-import pilot from "./pilot"
-import sponsor from "./sponsor"
-import event from "./event"
-
-export const schemaTypes: SchemaTypeDefinition[] = [article, pilot, sponsor, event]
